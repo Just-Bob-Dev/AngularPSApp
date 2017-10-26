052cf9965aaa3202a7fcf819919846c99dca1571
Robert Hutchins <rhutchins@reliaslearning.com>
2017-10-26 17:28:18 -0400

    added feature to angular app from module 6-8

---
+++ src/app/shared/star.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnChanges } from '@angular/core';
+
+@Component({
+    selector: 'pm-star',
+    templateUrl: './star.component.html',
+    styleUrls: ['./star.component.css']
+})
+
+export class StarComponent implements OnChanges {
+    rating: number = 4;
+    starWidth: number;
+
+    ngOnChanges(changes: SimpleChanges): void {
+        throw new Error("Method not implemented.");
+    }
+}
