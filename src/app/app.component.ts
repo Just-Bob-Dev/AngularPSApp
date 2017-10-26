e035b4587827675ecc95c82523634d5d353b6b90
Robert Hutchins <rhutchins@reliaslearning.com>
2017-10-26 10:29:22 -0400

    simple changes no defenition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-@Component({
+@Component ({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <div>
+    <h1>{{pageTitle}}</h1>
+    <h4>Not my first component</h4>
+  </div>
+  `
 })
+
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = 'Acme Product Management';
 }
