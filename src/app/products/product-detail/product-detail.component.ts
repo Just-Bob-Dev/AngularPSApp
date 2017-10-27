0308380ca0ab3281ab421be94638fc92e9a18b3e
Robert Hutchins <rhutchins@reliaslearning.com>
2017-10-27 14:49:19 -0400

    finished building out detailed product list.

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,14 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { IProduct } from '../product';
+import { ProductService } from '../product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+  pageTitle: string = 'Product Detail';
+  product: IProduct;
+  errorMessage: string = 'This is an error message';
 
-  constructor() { }
+  constructor(private _route: ActivatedRoute, private _router: Router, private _productService: ProductService) { }
 
   ngOnInit() {
+    let id = +this._route.snapshot.paramMap.get('id');
+    this._productService.getProduct(id)
+    .subscribe(
+        theProduct => {
+            this.product = theProduct;
+        },
+        error => this.errorMessage = <any>error);
+  }
+
+  onBack(): void {
+    this._router.navigate(['/products']);
   }
 
 }
